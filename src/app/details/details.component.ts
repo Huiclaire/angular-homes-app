b975b07a04269863b35fdfbc53f8e178c22aed42
Claire Lin <chunhuilin7516@gmail.com>
2024-07-16 14:16:39 +0200

    1. Add the form functions to the details page 2. Add the form's markup to the details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <article>
+    <article class="listing-all">
       <img class="listing-photo" [src]="housingLocation?.photo" alt="">
       <section class="list-description">
         <h2 class="listing-heading">{{housingLocation?.name}}</h2>
@@ -25,28 +26,55 @@ import { HousingLocation } from '../housing-location';
       </section>
       <section class="listing-apply">
         <h2 class="section-heading">Apply now to live here</h2>
-        <button class="primary" type="button">Apply now</button>
+        <!-- <button class="primary" type="button">Apply now</button> -->
+        <!-- 1. Replace the button with form element -->
+         <!-- 2. add submit for event binding -->
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName" />
+
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email" />
+
+          <button type="submit" class="primary">Apply</button>
+        </form>
       </section>
     </article>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-  // inject 'ActivatedRoute' into the component
+  // 1. Inject 'ActivatedRoute' into the component
   route: ActivatedRoute = inject(ActivatedRoute);
-  // Dependency injection
-  // 1.housingService is an instance of HousingService, which will be injected into the component.
-  // 2.housingLocation is a variable of type HousingLocation | undefined, which will
+  // 2. Dependency injection
+  // -housingService is an instance of HousingService, which will be injected into the component.
+  // -housingLocation is a variable of type HousingLocation | undefined, which will
   //   store the details of a specific housing location.
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
   // if housing location is undefined, the id property wont be accessed, to prevent error.
-
-
+  // 3. Define form model
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
   // Constructor initialization
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.
     getHousingLocationById(housingLocationId);
+  }
+
+  // Apply now click
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
